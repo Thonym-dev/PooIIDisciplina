421ca5ee6de2c64b87e03f45f88aaaea25a3c5da
thonym-dev <anthonyvitor534@gmail.com>
2024-10-03 02:01:13 -0300

    Commit teste

--- academico/src/main/java/br/edu/ifg/academico/DisciplinasApplication.java
+++ academico/src/main/java/br/edu/ifg/academico/DisciplinasApplication.java
@@ -3,7 +3,6 @@ package br.edu.ifg.academico;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class DisciplinasApplication {
 
